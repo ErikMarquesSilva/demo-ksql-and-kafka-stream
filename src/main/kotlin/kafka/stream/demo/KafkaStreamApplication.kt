ba72057630cd1c275bfd936fb3c9bfb39bf453d2
Danilo Paiva <danilopaivasilva@gmail.com>
2020-02-11 17:34:25 -0300

    initial development

---
+++ src/main/kotlin/kafka/stream/demo/KafkaStreamApplication.kt
@@ -0,0 +1,11 @@
+package kafka.stream.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KafkaStreamApplication
+
+fun main(args: Array<String>) {
+	runApplication<KafkaStreamApplication>(*args)
+}
